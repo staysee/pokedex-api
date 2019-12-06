c84bb9101cac709bbbbdd6c43a1579733cdc5c91
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-12-05 16:24:31 -0800

    validating bearer token

--- server.js
+++ server.js
@@ -10,14 +10,35 @@ app.use(morgan('dev'));
 //valid types of pokemon
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+
+    console.log('validate bearer token middleware')
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken){
+        return res.status(401).json({ error: 'Unauthorized request' })
+    }
+    //move to the next middleware
+    next()
+})
+
 function handleGetTypes(req, res) {
     res.json(validTypes)
-
 }
 
 app.get('/types', handleGetTypes)
 
-const PORT = 8000;
+function handleGetPokemon(req, res) {
+    res.send('Hello, Pokemon!')
+}
+
+app.get('/pokemon', handleGetPokemon)
+
+
+
+
+const PORT = 8080;
 
 app.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`)
