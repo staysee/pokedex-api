60ec3fe712a22ec6f7df33eebebe250e226e9556
Stacey Sugiono <sugiono.stacey@gmail.com>
2019-12-05 16:37:33 -0800

    connecting to pokedex.json

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
+const POKEDEX = require('./pokedex.json')
 
-console.log(process.env.API_TOKEN)
+const app = express();
 
 app.use(morgan('dev'));
 
-//valid types of pokemon
-const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+console.log(process.env.API_TOKEN)
 
 app.use(function validateBearerToken(req, res, next) {
     const apiToken = process.env.API_TOKEN
     const authToken = req.get('Authorization')
-
+    
     console.log('validate bearer token middleware')
-
+    
     if (!authToken || authToken.split(' ')[1] !== apiToken){
         return res.status(401).json({ error: 'Unauthorized request' })
     }
@@ -23,19 +22,33 @@ app.use(function validateBearerToken(req, res, next) {
     next()
 })
 
-function handleGetTypes(req, res) {
+//valid types of pokemon
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
+
+app.get('/types', function handleGetTypes(req, res) {
     res.json(validTypes)
-}
+})
 
-app.get('/types', handleGetTypes)
+app.get('/pokemon', function handleGetPokemon(req, res) {
+    let response = POKEDEX.pokemon;
 
-function handleGetPokemon(req, res) {
-    res.send('Hello, Pokemon!')
-}
+    //filter our pokemon by name if name query param is present
+    if (req.query.name) {
+        response = response.filter( pokemon => 
+            // case insensitive searching
+            pokemon.name.toLowerCase().includes(req.query.name.toLowerCase())
+        )
+    }
 
-app.get('/pokemon', handleGetPokemon)
+    //filter our pokemon by type if type query param is present
+    if (req.query.type) {
+        response = response.filter(pokemon =>
+            pokemon.type.includes(req.query.type)
+        )
+    }
 
-
+    res.json(response)
+})
 
 
 const PORT = 8080;
